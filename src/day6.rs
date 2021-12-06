e350cf5b0c6fc96c423b53c938509b87c8874fd5
Benjamin Andersson <4161184+bennyy@users.noreply.github.com>
2021-12-06 18:59:11 +0100

    Day 6 completed

--- src/day6.rs
+++ src/day6.rs
@@ -2,20 +2,43 @@ use crate::file_util;
 
 static INPUT_FILE: &str = "inputs/day6.txt";
 
-fn puzzle1(file_path: String) -> u32 {
+fn puzzle(file_path: String, days: u32) -> u64 {
     let input_str = file_util::file_to_string(file_path.clone());
-    let input_lines: Vec<&str> = input_str.lines().collect();
-    return 0;
-}
+    let input_lines: Vec<usize> = input_str
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
 
-fn puzzle2(file_path: String) -> u32 {
-    return 0;
+    // Convert the data to a "map" where the key is the timer and the value is how many fishes
+    let mut lanternfishes = vec![0; 9];
+    for fish in input_lines {
+        *lanternfishes.get_mut(fish).unwrap() += 1;
+    }
+
+    for _ in 0..days {
+        // Split the array in to two parts. Old fishes and the new fishes.
+        let (old_fishes, new_fishes) = lanternfishes.split_at_mut(7);
+
+        // Save all data before it shifted away and replaced and edited.
+        let fishes_to_spawn = *old_fishes.get(0).unwrap();
+        let fishes_to_merge = *new_fishes.get(0).unwrap();
+
+        // Shift the list one step to the left
+        old_fishes.rotate_left(1);
+        new_fishes.rotate_left(1);
+
+        // Add the new fishes from 7 timer-fishes into 6 timer-fishes position.
+        *old_fishes.get_mut(6).unwrap() += fishes_to_merge;
+        // All 0 timer-fishes, spawn fishes in timer 8.
+        *new_fishes.get_mut(1).unwrap() = fishes_to_spawn;
+    }
+    return lanternfishes.iter().sum();
 }
 
 pub fn run() {
-    let result = puzzle1(INPUT_FILE.to_string());
+    let result = puzzle(INPUT_FILE.to_string(), 80);
     println!("D6P1: {}", result);
-    let result = puzzle2(INPUT_FILE.to_string());
+    let result = puzzle(INPUT_FILE.to_string(), 256);
     println!("D6P2: {}", result);
 }
 
@@ -26,13 +49,13 @@ mod tests {
 
     #[test]
     fn puzzle_1() {
-        assert_eq!(1, puzzle1(TEST_INPUT_FILE.to_string()));
-        assert_eq!(1, puzzle1(INPUT_FILE.to_string()));
+        assert_eq!(5934, puzzle(TEST_INPUT_FILE.to_string(), 80));
+        assert_eq!(374927, puzzle(INPUT_FILE.to_string(), 80));
     }
 
     #[test]
     fn puzzle_2() {
-        assert_eq!(1, puzzle2(TEST_INPUT_FILE.to_string()));
-        assert_eq!(1, puzzle2(INPUT_FILE.to_string()));
+        assert_eq!(26984457539, puzzle(TEST_INPUT_FILE.to_string(), 256));
+        assert_eq!(1687617803407, puzzle(INPUT_FILE.to_string(), 256));
     }
 }
